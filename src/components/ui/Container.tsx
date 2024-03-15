bbfdbf24b15aa1a0835a5eff26bac960eb802d8b
ldemyanov <leonidemyanov@gmail.com>
2024-03-15 22:06:41 +0800

    feat: first deploy

--- src/components/ui/Container.tsx
+++
@@ -1,9 +0,0 @@
-type ContainerProps = {
-  children: React.ReactNode;
-};
-
-const Container = ({ children }: ContainerProps) => {
-  return <div className="md:container mx-auto">{children}</div>;
-};
-
-export default Container;
--- src/utils/getId.ts
+++
@@ -1,5 +0,0 @@
-import { MD5 } from "crypto-js";
-
-export const getId = (text: string) => {
-  return MD5(`${Date.now()}${text}${Date.now()}`).toString();
-}
